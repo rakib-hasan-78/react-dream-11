aa80c301e218b6765dca86c6741d754c37ef4819
rakib-hasan-78 <rakibwebdev78@gmail.com>
2025-03-04 16:03:42 +0600

    selection responsive done

--- src/components/Selection.jsx
+++ src/components/Selection.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const Selection = ()=> {
     return (
         <div className='w-full h-auto flex flex-wrap items-center justify-center content-center py-10 mt-5'>
-            <div className='w-full h-auto flex items-center justify-between'>
+            <div className='w-full h-auto flex flex-wrap items-center 3xs:justify-center 3xs:space-y-5 lg:justify-between'>
                 <h3>Available Players</h3>
                 <div className='w-fit flex items-center justify-end border space-x-5 rounded-xl'>
                     <button className='btn'>available</button>
